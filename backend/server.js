f6c9c2a47e7e11e23a97878afe73c39adf987ee4
ksingh408 <92683960+ksingh408@users.noreply.github.com>
2025-09-05 16:51:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const server = createServer(app);
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({ 
-   // origin: "http://localhost:5173",
+   
      origin: "https://chat-app-orpin-tau.vercel.app", 
      credentials: true })
     );
@@ -34,7 +34,7 @@ app.use('/api/msg', messageRoutes);
 
 // Socket.io
 const io = new Server(server, { cors: {
-     //origin: "http://localhost:5173", 
+     
      origin: "https://chat-app-orpin-tau.vercel.app",
     credentials: true } });
 
