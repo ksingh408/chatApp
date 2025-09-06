661ed1b1b0bea979bdadd03ac91de2f1a4f33974
ksingh408 <kunalsinghxy@gmail.com>
2025-09-07 01:08:27 +0530

    fixsocketImport

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,35 +1,5 @@
 
 
-
-
-import { io } from "socket.io-client";
-
-let socket;
-
-export const connectSocket = async () => {
-  if (!socket) {
-    socket = io(
-      // "http://localhost:5000",
-      "https://chatapp-s96a.onrender.com" ,{
-      transports: ["websocket"],
-      withCredentials: true,
-      autoConnect: false, // manual connect
-    });
-
-    await new Promise((resolve) => {
-      socket.connect();
-      socket.on("connect", resolve);
-    });
-
-    console.log("Socket is actually connected now, ID:", socket.id);
-  }
-
-  return socket;
-};
-
-export const getSocket = () => socket;
-
-
 // import { io } from "socket.io-client";
 
 // let socket;
