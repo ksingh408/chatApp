02235dde800356e3351708919f06bade7a0bed19
ksingh408 <kunalsinghxy@gmail.com>
2025-09-07 00:58:48 +0530

    updateApi

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,9 +1,48 @@
 import axios from "axios";
 
+import { io } from "socket.io-client";
+
 const publicAPI = axios.create({
-  baseURL: "http://localhost:5000/api", // only the base
-  // baseURL:"https://chatapp-s96a.onrender.com"
+  baseURL : import.meta.env.VITE_API_URL,
+  
   withCredentials: true
 });
 
 export default publicAPI;
+
+
+
+
+
+let socket;
+
+export const connectSocket = async () => {
+  if (!socket) {
+    socket = io(
+      import.meta.env.VITE_API_URL.replace("/api", ""),
+  
+      {
+      transports: ["websocket"],
+      withCredentials: true,
+      autoConnect: false, // manual connect
+    });
+
+    await new Promise((resolve) => {
+      socket.connect();
+      socket.on("connect", resolve);
+    });
+
+    console.log("Socket is actually connected now, ID:", socket.id);
+  }
+
+  return socket;
+};
+
+export const getSocket = () => socket;
+
+
+
+
+
+
+
