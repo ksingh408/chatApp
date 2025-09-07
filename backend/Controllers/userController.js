27fb1dcef01369ccd731260d96f8877075019ef7
ksingh408 <92683960+ksingh408@users.noreply.github.com>
2025-09-07 17:48:42 +0530

    Update userController.js

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -87,8 +87,8 @@ const login = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false, // true in production with HTTPS
-            sameSite: "lax",
+            secure: true, // true in production with HTTPS
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
 
