282b4d07de2beaf9018d040d535a2060b62e62ca
ksingh408 <kunalsinghxy@gmail.com>
2025-09-26 15:49:18 +0530

    feat(auth): improve register API and generate JWT cookie

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -43,6 +43,21 @@ const register = async (req, res) => {
 
         await newUser.save();
 
+        
+        const token = jwt.sign(
+            { id: existingUser._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "7d" }
+        );
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: false, // true in production with HTTPS
+            sameSite: "lax",
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+        });
+
+
         res.status(201).json({
             message: 'User registered successfully',
             user: {
