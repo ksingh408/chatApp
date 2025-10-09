9666de32355968dc23d30cb96562dd5cd60dc14f
ksingh408 <92683960+ksingh408@users.noreply.github.com>
2025-10-09 17:54:16 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -21,17 +21,17 @@ export const publicAPI = axios.create({
 // Optional: Interceptors for error handling
 
 
-publicAPI.interceptors.request.use(
-  (config)=>{
-    const token = localStorage.getItem('token');
-    if(token){
-      config.headers.Authorization =`Bearer ${token}`;
-    }
-    return config;
-  },
+// publicAPI.interceptors.request.use(
+//   (config)=>{
+//     const token = localStorage.getItem('token');
+//     if(token){
+//       config.headers.Authorization =`Bearer ${token}`;
+//     }
+//     return config;
+//   },
 
-  (error)=> Promise.reject(error)
-)
+//   (error)=> Promise.reject(error)
+// )
 
 publicAPI.interceptors.response.use(
 
