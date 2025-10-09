5fdb8e2633e911d89f0d113c58425e97e7b1850e
ksingh408 <kunalsinghxy@gmail.com>
2025-10-09 13:11:56 +0530

    Resolved conflict and applied cherry-pick

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,17 +1,20 @@
 // ---------------------ChatPage.jsx------------------------
 
-import React, { useState, useEffect, useRef, useCallback } from "react";
-import { useSelector } from "react-redux";
 
+
+// ChatPage.jsx
+import React, { useState, useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+import  {connectSocket, getSocket } from "../api/api.js";
 import FriendList from "../component/friendList";
 import ChatWindow from "../component/chatWindow";
-
-import { connectSocket, getSocket } from "../api/api.js";
-import { publicAPI } from "../api/api.js";
+import {publicAPI} from "../api/api.js";
+import { useCallback } from "react";
 
 const ChatPage = () => {
   const reduxUser = useSelector((state) => state.auth.user);
   const userId = reduxUser?._id || reduxUser?.id;
+  const dispatach = useDispatch();
 
   const [friends, setFriends] = useState([]);
   const [selectedFriend, setSelectedFriend] = useState(null);
@@ -125,39 +128,30 @@ const ChatPage = () => {
       setShowFriendList(false);
 
       try {
-  
-      // const res = await publicAPI.get(`/msg/${friend._id}`);
-      // console.log(res.data);
-      // const formattedMessages = res.map((m) => ({
-      //   text: m.text,
-      //   senderId: m.sender,
-      //   receiverId: m.receiver,
-      //   roomId: m.conversationId,
-      //   createdAt: m.createdAt,
-      // }));
-      // setMessages(formattedMessages);
+        const res = await publicAPI.get(`/msg/${friend._id}`);
+        console.log(res);
 
         const formattedMessages = res.map((m) => ({
-        text: m.text,
-        senderId: m.sender,
-        receiverId: m.receiver,
-        roomId: m.conversationId,
-        createdAt: m.createdAt,
-      }));
-      setMessages(formattedMessages);
+          text: m.text,
+          senderId: m.sender,
+          receiverId: m.receiver,
+          roomId: m.conversationId,
+          createdAt: m.createdAt,
+        }));
+        setMessages(formattedMessages);
 
-      const roomId = [userId, friend._id].sort().join("_");
+        const roomId = [userId, friend._id].sort().join("_");
 
         if (!socketRef.current || !socketRef.current.connected) {
-        socketRef.current = await connectSocket();
-      }
+          socketRef.current = await connectSocket();
+        }
 
-      socketRef.current.emit("joinRoom", roomId);
-      socketRef.current.currentRoom = roomId;
-    } catch (err) {
-      console.error("Error fetching messages:", err);
-      setMessages([]);
-    }
+        socketRef.current.emit("joinRoom", roomId);
+        socketRef.current.currentRoom = roomId;
+      } catch (err) {
+        console.error("Error fetching messages:", err);
+        setMessages([]);
+      }
     },
     [userId]
   );
